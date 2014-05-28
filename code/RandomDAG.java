d601b1104ede39adb999cf8371c1188370cbca26
ezql <caleb.fetzer@gmail.com>
2014-05-28 10:57:58 +0800

    Made RandomDAg, edited report
    Made RandomDAg, edited reportMade RandomDAg, edited reportMade
    RandomDAg, edited reportMade RandomDAg, edited report

--- code/RandomDAG.java
+++ code/RandomDAG.java
@@ -3,35 +3,57 @@
  * Generates a graph from 1-10000
  */
 import java.util.*; // for Random, Set, List, Map
+import java.lang.IllegalArgumentException;
 
-public class RanDagGen {
+public class RandomDAG {
 
 	public static void main (String[] args) {
 	
-	DirAcycGraph dm = new DirAcycGraph();
-	int keySize = args[0];			//amount of verticies in graph
-	int density = args[1];			//amount of dependencies
-	
-	if (density > keySize) {
-		throw new IllegalArgumentException ("Density cannot be greater than keysize.");
-	} else {
-		Random r  = new Random();
-		//Put given amount of 
-		for (int i = 0; i < keySize; i++){
-			dm.put(i, "Value");
-			if r.nextInt(i) = i
-				dm.put(i, "Wow..lucky");
+		if(args.length==2)
+		{
+			int density = 0;
+			int keySize = 0;
+			DirAcycGraph dm = new DirAcycGraph();
+
+			try {
+				keySize = Integer.parseInt(args[0]);
+				density = Integer.parseInt(args[1]);
+				}			//amount of verticies in graph
+				catch(NumberFormatException e)
+				{//error}
+					System.exit(0);
+				}
+
+				Key[] keyArray = new Key[keySize+1];
+			
+			if (density > keySize) {
+				throw new IllegalArgumentException ("Density cannot be greater than keysize.");
+			} else {
+				Random r  = new Random();
+				//Put given amount of 
+				for (int i=0;i<keySize;i++)
+				{
+					Key key = new Key();
+					keyArray[i] = key;
+				}
+				//int val;
+				for(Key i : keyArray)
+				{
+					dm.put(i, "Value");
+					/*if (r.nextInt() = i)
+						dm.put(i, "Wow..lucky");*/
+				}
+			
+				/*for (Key i : density) {
+					Key<Integer> val = r.nextInt(i);
+					if (!isDependent(val, i))
+						dm.addDependency(val, i);		
+					else i++;
+				}*/
+
+			}
 
 		}
-	
-		for (int i = 0; i < density; i++) {
-			int val = r.nextInt(i);
-			if (!isDependent(val, i)
-				(dm.addDependency(val, i));			
-			else i++;
-		}
-
 	}
-
 
 }
